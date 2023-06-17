7a90b5279f0154c9ce39a6ba48e8df1c74c2f842
Vishakha Shetye <vishakhashetye@Vishakhas-MacBook-Air.local>
2023-06-17 18:01:58 +0530

    Created server app and added / rest api to show hello message

---
+++ ImageAnnotatorServer/src/test/java/com/vs/ImageAnnotatorServer/ImageAnnotatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.ImageAnnotatorServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAnnotatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
